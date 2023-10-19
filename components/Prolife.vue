67fb476baae62a38aaf3684e776da4491fe6eb25
弓矢 <yumiyachizuru@gmail.com>
2023-10-19 12:12:39 +0900

    プロフィール欄

---
+++ components/Prolife.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="profile">
+    <div class="profile_photo">
+        <img src="/avatar.jpg" alt="avatar" />
+    </div>
+    <div class="profile_content">
+      <h3 class="h3">弓矢</h3>
+      <p class="profile_copy">たのしい場所を作りたいWebエンジニアです。</p>
+      <ul class="profile_list">
+        <li>オタク文化わかります</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.profile {
+  display: flex;
+  justify-content: center;
+}
+@media screen and (max-width: 768px) {
+  .profile {
+    flex-direction: column;
+    align-items: center;
+  }
+}
+.profile_photo {
+  margin-right: 20px;
+  width: 150px;
+  aspect-ratio: 1;
+}
+@media screen and (max-width: 768px) {
+  .profile_photo {
+    margin-right: 0;
+  }
+}
+.profile_photo img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 50%;
+}
+
+.profile_content {
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
     <Header />
+    <div class="contents">
+      <div class="content">
+        <h2 class="h2">Prolife</h2>
+        <Prolife />
+      </div>
+    </div>
   </div>
 </template>
