54af3b4e7b19deada94a165569f8b7d0bd6e8565
弓矢 <yumiyachizuru@gmail.com>
2023-10-25 18:30:53 +0900

    microCMSのtarget属性を修正

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,8 @@ export default defineNuxtConfig({
       {
         serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
         apiKey: process.env.MICROCMS_API_KEY,
+        target: 'all',
       },
-    ],
+    ]
   ]
 })
